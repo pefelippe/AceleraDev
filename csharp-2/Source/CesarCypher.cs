dc19391bfd5190e9d11172f2cbcad0f2d63c0899
pedro <pedfelippe@gmail.com>
2020-05-14 14:27:35 -0300

    Inicio do Desafio 02

--- csharp-2/Source/CesarCypher.cs
+++ csharp-2/Source/CesarCypher.cs
@@ -1,17 +1,44 @@
 using System;
+using System.Diagnostics.Tracing;
+using System.Linq.Expressions;
 
 namespace Codenation.Challenge
 {
     public class CesarCypher : ICrypt, IDecrypt
     {
+
         public string Crypt(string message)
         {
-            throw new NotImplementedException();
+            message = message.ToLower();
+
+            char[] msg = message.ToCharArray();
+            // a = 97 e z = 122
+            // ultimos numeros tem q voltar pro começo
+            // ignorar espaço
+            foreach(char c in msg)
+            {
+                int converter = c;
+                if(converter >= 97 && converter <= 122)
+                {
+                    converter += 3;
+                    
+                }
+                char cripted = (char)converter;
+                Console.Write(cripted);
+            }
+
+            string cryptedMsg = msg.ToString();
+
+            return cryptedMsg;
         }
 
         public string Decrypt(string cryptedMessage)
         {
-            throw new NotImplementedException();
+            cryptedMessage = cryptedMessage.ToLower();
+            string decriptedMessage = " ";
+            return decriptedMessage;
         }
+
+
     }
 }
---
+++ csharp-2/myProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using Codenation.Challenge;
+using System;
+
+namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CesarCypher cypher = new CesarCypher();
+
+            string msgTeste = "the quick brown fox jumps over the lazy dog";
+
+            msgTeste = cypher.Crypt(msgTeste);
+            //Console.WriteLine("Encriptografada: " + msgTeste);
+
+            //msgTeste = cypher.Decrypt(msgTeste);
+            //Console.WriteLine("Decriptografada: " + msgTeste);
+        }
+    }
+}
