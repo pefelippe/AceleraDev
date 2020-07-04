7b5e8e6f207bd51c112d371b9f26b726c3ba1358
Pedro Felippe <pedrofelippe@alu.ufc.br>
2020-07-04 20:35:39 -0300

    ChallengeService + SubmissionsService - ok

--- csharp-8/Source/Services/ChallengeService.cs
+++ csharp-8/Source/Services/ChallengeService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
 
 namespace Codenation.Challenge.Services
@@ -6,19 +7,37 @@ namespace Codenation.Challenge.Services
     public class ChallengeService : IChallengeService
     {
         CodenationContext _context;
+
         public ChallengeService(CodenationContext context)
         {
             _context = context;
         }
 
-        public IList<Models.Challenge> FindByAccelerationIdAndUserId(int accelerationId, int userId)
+        public IList<Models.Challenge> FindByAccelerationIdAndUserId(int accelerationId, int userId) // ret uma lista de desafios
         {
-            throw new System.NotImplementedException();
-        }
+            return _context.Candidates
+                .Where(x => x.UserId == userId && x.AccelerationId == accelerationId)
+                .Select(x => x.Acceleration)
+                .Select(x => x.Challenge)
+                .Distinct()
+                .ToList();
 
+        }
         public Models.Challenge Save(Models.Challenge challenge)
         {
-            throw new System.NotImplementedException();
+            if (challenge.Id == 0)
+            {
+                _context.Challenges.Add(challenge);
+                _context.SaveChanges();
+                return _context.Challenges.Last();
+            }
+            else
+            {
+                var update = _context.Challenges.Where(x => x.Id == challenge.Id).SingleOrDefault();
+                update = challenge;
+                _context.SaveChanges();
+                return update;
+            }
         }
     }
 }
--- csharp-8/Source/Services/SubmissionsService.cs
+++ csharp-8/Source/Services/SubmissionsService.cs
@@ -1,29 +1,49 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Codenation.Challenge.Models;
 
 namespace Codenation.Challenge.Services
 {
     public class SubmissionService : ISubmissionService
     {
-        CodenationContext _context;
+        private readonly CodenationContext _context;
         public SubmissionService(CodenationContext context)
         {
             _context = context;
         }
-
-        public IList<Submission> FindByChallengeIdAndAccelerationId(int challengeId, int accelerationId)
+        public IList<Submission> FindByChallengeIdAndAccelerationId(int challengeId, int accelerationId) //corr��o de erros
         {
-            throw new System.NotImplementedException();
-        }
+            return _context.Candidates.Where(x => x.AccelerationId == accelerationId)
+                                      .Select(x => x.User)
+                                      .SelectMany(x => x.Submissions)
+                                      .Where(x => x.ChallengeId == challengeId).Distinct().ToList();
 
+        }
         public decimal FindHigherScoreByChallengeId(int challengeId)
         {
-            throw new System.NotImplementedException();
+            return _context.Submissions
+                        .Where(x => x.ChallengeId == challengeId)
+                        .OrderByDescending(x => x.Score)
+                        .FirstOrDefault()
+                        .Score;
         }
-
         public Submission Save(Submission submission)
         {
-            throw new System.NotImplementedException();
+            if (!(_context.Submissions.Any(x => x.UserId == submission.UserId && x.ChallengeId == submission.ChallengeId))) //not exist 
+            {
+                _context.Submissions.Add(submission);
+                _context.SaveChanges();
+                return _context.Submissions.Last();
+            }
+            else
+            {
+                var update = _context.Submissions
+                             .Where(x => x.UserId == submission.UserId && x.ChallengeId == submission.ChallengeId)
+                             .SingleOrDefault();
+                update = submission;
+                _context.SaveChanges();
+                return update;
+            }
         }
     }
 }
